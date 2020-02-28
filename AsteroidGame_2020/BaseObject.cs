23f027732d6b5d87309f5feec70244e118cc8064
Andrey Gritsenko <a.gritsenko87@yandex.ru>
2020-02-29 01:49:01 +0300

    Задание № 2

--- AsteroidGame_2020/BaseObject.cs
+++ AsteroidGame_2020/BaseObject.cs
@@ -20,18 +20,11 @@ namespace AsteroidGame_2020
             Size = size;
         }
         public abstract void Draw(); // в абстрактном базовом классе методы должны быть без реализации, вся реализация идет в производных классах
-        public virtual void Update()
-        {
-            Pos.X = Pos.X + Dir.X;
-            Pos.Y = Pos.Y + Dir.Y;
-            if (Pos.X < 0) Dir.X = -Dir.X;
-            if (Pos.X > Game.Width) Dir.X = -Dir.X;
-            if (Pos.Y < 0) Dir.Y = -Dir.Y;
-            if (Pos.Y > Game.Height) Dir.Y = -Dir.Y;
-        }
-        // Так как переданный объект тоже должен будет реализовывать интерфейс ICollision, мы 
-        // можем использовать его свойство Rect и метод IntersectsWith для обнаружения пересечения с
-        // нашим объектом (а можно наоборот)
+        public abstract void Update();
+
+        //Так как переданный объект тоже должен будет реализовывать интерфейс ICollision, мы
+        //можем использовать его свойство Rect и метод IntersectsWith для обнаружения пересечения с
+        //нашим объектом(а можно наоборот)
         public bool Collision(ICollision o) => o.Rect.IntersectsWith(this.Rect);
         public Rectangle Rect //public Rectangle Rec => new Rectangle(Pos, Size); можно вот так сократить
         {
