0c661d0278db816f9be0dd61510b48598ec7a87f
Andrey Gritsenko <a.gritsenko87@yandex.ru>
2020-03-02 23:59:51 +0300

    Создаем класс Ship

---
+++ AsteroidGame_2020/Ship.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AsteroidGame_2020
+{
+    class Ship : BaseObject
+    {
+        private int _energy = 100;
+        public int Energy => _energy;
+        public Ship(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.FillEllipse(Brushes.Wheat, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+        public override void Update()
+        {
+
+        }
+        public void Up()
+        {
+            if (Pos.Y > 0) Pos.Y = Pos.Y - Dir.Y;
+        }
+        public void Down()
+        {
+            if (Pos.Y < Game.Height) Pos.Y = Pos.Y + Dir.Y;
+        }
+        public void Die()
+        {
+
+        }
+    }
+}
