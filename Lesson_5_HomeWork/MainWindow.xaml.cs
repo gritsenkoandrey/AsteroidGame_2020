a88b4058bfb949a11397b38d7fd408a9785e784b
Andrey Gritsenko <a.gritsenko87@yandex.ru>
2020-03-11 00:11:50 +0300

    Готовый результат, правда очень не красивый

--- Lesson_5_HomeWork/MainWindow.xaml.cs
+++ Lesson_5_HomeWork/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Lesson_5_HomeWork.Work;
+using System.ComponentModel;
 
 namespace Lesson_5_HomeWork
 {
@@ -20,14 +22,27 @@ namespace Lesson_5_HomeWork
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BindingList<WorkClass> _workClasses;
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            _workClasses = new BindingList<WorkClass>()
+            {
+                new WorkClass(){ Employee = "Andrey Gritsenko", Department = "geekbrains"},
+                new WorkClass(){ Employee = "Dmitriy Kiselev", Department = "geekbrains"},
+                new WorkClass(){ Employee = "Irina Gritsenko", Department = "geekbrains"}
+            };
+            ed_Homework.ItemsSource = _workClasses;
+            //_workClasses.ListChanged += _workClasses_ListChanged;
         }
+
+        //private void _workClasses_ListChanged(object sender, ListChangedEventArgs e)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
     }
 }
---
+++ Lesson_5_HomeWork/Work/WorkClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5_HomeWork.Work
+{
+    class WorkClass
+    {
+        private string _employee;
+        private string _department;
+        public string Employee
+        {
+            get { return _employee; }
+            set
+            {
+                _employee = value;
+            }
+        }
+        public string Department
+        {
+            get { return _department; }
+            set
+            {
+                _department = value;
+            }
+        }
+    }
+}
