69bf1743a6df87778171870408e0765728cca284
Andrey Gritsenko <a.gritsenko87@yandex.ru>
2020-03-15 23:20:30 +0300

    Метод ExecuteNonQuery

--- DataBaseLesson-7/Program.cs
+++ DataBaseLesson-7/Program.cs
@@ -19,7 +19,26 @@ namespace DataBaseLesson_7
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
+
+                //SqlCommand command = new SqlCommand(createExpression, connection);
+                //command.ExecuteNonQuery(); // выполнили sql запрос, если запрос один раз выполнен, то выскочит ошибка
+
+                //var command = new SqlCommand(sqlExpression, connection);
+                //command.ExecuteNonQuery();
+
+
             }
         }
+
+        private const string  createExpression = @"CREATE TABLE[dbo].[People] (
+                                    [Id] INT IDENTITY(1, 1) NOT NULL,
+                                    [FIO] NVARCHAR(MAX) COLLATE Cyrillic_General_CI_AS NOT NULL,
+                                    [Birthday] NVARCHAR(MAX) NULL,
+                                    [Email]    NVARCHAR(100) NULL,
+                                    [Phone]    NVARCHAR(MAX) NULL,
+                                    CONSTRAINT[PK_dbo.People] PRIMARY KEY CLUSTERED([Id] ASC));"; // sql запрос
+
+        private const string sqlExpression = @"INSERT INTO People (FIO, Birthday,Email,Phone) VALUES ( N'Иванов Иван Иванович', '18.10.2001', 'somebody@gmail.com', '89164444444' );
+                                     INSERT INTO People(FIO, Birthday, Email, Phone) VALUES( N'Петров Петр Петрович', '15.01.2001', 'somebody@mail.com', '8916555555')";
     }
 }
