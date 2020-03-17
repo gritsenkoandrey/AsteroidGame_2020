53777d30a0f3a9cd473c96df13f0a8fdf1e51790
Andrey Gritsenko <a.gritsenko87@yandex.ru>
2020-03-17 23:01:28 +0300

    Рефакторинг

---
+++ Procedure_WPF.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[procedure]
+    @employee nvarchar(50),
+    @department nvarchar(50),
+    @Id int out
+AS
+    INSERT INTO datadataGrid(Employee, Department)
+    VALUES (@employee, @department)
+   
+    SET @Id=SCOPE_IDENTITY()
+GO
